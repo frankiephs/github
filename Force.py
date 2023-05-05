c32c319077f01744648c5bd8504819c85af91922
frankiephs <132312999+frankiephs@users.noreply.github.com>
2023-05-05 03:04:57 +0000

    Sample from replit commit
    Sample description from replit commit

--- Force.py
+++ Force.py
@@ -1,5 +1,14 @@
-Acceleration = float(input("Add an acceleration"))
-Mass = float(input("Add mass"))
+
+# Name
+username =  str(input("Enter your name! "))
+
+# Greetings
+print("Welcome {}! This is the Force calculator made by franklin".format(username))
+
+# Calculation
+Acceleration = float(input("Add an acceleration (ms^2) : "))
+Mass = float(input("Add mass (kg) : "))
 Force = Mass * Acceleration 
 
-print(str(Force) + " " + "N")
+# Results
+print("Succesfull!" + " " + str(Force) + "N" + " " + "=" + " " + str(Mass) + " " + "x" + " " + str(Acceleration))
